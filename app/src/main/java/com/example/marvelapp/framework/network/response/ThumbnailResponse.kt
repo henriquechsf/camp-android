95e2abcd743bce892b9edcbe977f594bc4068b00
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-10-22 01:58:54 -0300

    implementa CharactersPagingSource

--- app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
@@ -4,5 +4,5 @@ data class ThumbnailResponse(
     val path: String,
 
     // @SerializedName("extension") - caso precise mapear nome de atributo diferente
-    val extensionPath: String
+    val extension: String
 )
