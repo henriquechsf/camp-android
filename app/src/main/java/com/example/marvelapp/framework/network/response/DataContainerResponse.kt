63c9f6da639d5e5580782cb70bd103c70f2f7d94
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-11-02 16:58:13 -0300

    config build variantes debug, staging e release

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,7 +1,14 @@
 package com.example.marvelapp.framework.network.response
 
+import com.google.gson.annotations.SerializedName
+
 data class DataContainerResponse(
+    @SerializedName("results")
     val results: List<CharacterResponse>,
+
+    @SerializedName("offset")
     val offset: Int,
+
+    @SerializedName("total")
     val total: Int
 )
