402fc52b2f259a634a229ababd217e5fccc86a96
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-06 11:29:15 -0300

    configuracao MockWebServer

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -8,10 +8,16 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.marvelapp.R
+import com.example.marvelapp.extension.asJsonString
+import com.example.marvelapp.framework.di.BaseUrlModule
 import com.example.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.hamcrest.Matcher
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -19,6 +25,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@UninstallModules(BaseUrlModule::class)
 @HiltAndroidTest
 class CharactersFragmentTest {
 
@@ -26,15 +33,28 @@ class CharactersFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    private lateinit var server: MockWebServer
+
     // Inicializa um fragmento em uma activity vazia
     @Before
     fun setup() {
+        server = MockWebServer().apply {
+            start(8080)
+        }
         launchFragmentInHiltContainer<CharactersFragment>() // usa config do arquivo HiltExt
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
     }
 
     @Test
     fun shouldShowCharacters_whenViewIsCreated() {
         //onView(isRoot()).perform(waitFor(3000))
+
+        // mocka o retorno da api com dados de arquivo json da pasta assets
+        server.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
 
         // verifica se o recycler view esta sendo exibido na tela
         onView(withId(R.id.recycler_characters))
@@ -43,6 +63,7 @@ class CharactersFragmentTest {
             )
     }
 
+    /*
     private fun waitFor(delay: Long): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
@@ -52,4 +73,5 @@ class CharactersFragmentTest {
             }
         }
     }
+     */
 }
