d6bdbc68d8abbce7e9714e248e4fa78db2454150
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-07 13:49:58 -0300

    fetch comics e eventos paralalamente com coroutines

--- testing/src/main/java/tech/henriquedev/testing/model/CharacterFactory.kt
+++ testing/src/main/java/tech/henriquedev/testing/model/CharacterFactory.kt
@@ -6,8 +6,12 @@ class CharacterFactory {
 
     companion object {
         fun create(hero: Hero) = when (hero) {
-            Hero.ThreeDMan -> Character("3-D Man", "https://any-url.jpg")
-            Hero.ABomb -> Character("A-Bomb", "https://any-url.jpg")
+            Hero.ThreeDMan -> Character(
+                id = 100,
+                name = "3-D Man",
+                imageUrl = "https://any-url.jpg"
+            )
+            Hero.ABomb -> Character(id = 101, name = "A-Bomb", imageUrl = "https://any-url.jpg")
         }
     }
 
