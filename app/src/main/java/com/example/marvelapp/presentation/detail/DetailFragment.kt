d640a65dadace3ed00d8d92ba2eb5fd003b294c3
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-10 01:06:05 -0300

    react ui state on favorite

--- app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -43,13 +43,23 @@ class DetailFragment : Fragment() {
 
         binding.imageCharacter.run {
             transitionName = detailViewArg.name
-
             imageLoader.load(imageView = this, imageUrl = detailViewArg.imageUrl)
         }
 
         // transicao animacao da imagem
         setSharedElementTransitionOnEnter()
 
+        observeUiState(detailViewArg)
+        observeFavoriteUiState()
+
+        viewModel.getCharacterCategories(detailViewArg.characterId)
+
+        binding.imageFavoriteIcon.setOnClickListener {
+            viewModel.updateFavorite(detailViewArg)
+        }
+    }
+
+    private fun observeUiState(detailViewArg: DetailViewArg) {
         viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
             binding.flipperDetail.displayedChild = when (uiState) {
                 DetailViewModel.UiState.Loading -> FLIPPER_CHILD_POSITION_LOADING
@@ -69,8 +79,18 @@ class DetailFragment : Fragment() {
                 DetailViewModel.UiState.Empty -> FLIPPER_CHILD_POSITION_EMPTY
             }
         }
+    }
 
-        viewModel.getCharacterCategories(detailViewArg.characterId)
+    private fun observeFavoriteUiState() {
+        viewModel.favoriteUiState.observe(viewLifecycleOwner) { favoriteUiState ->
+            binding.flipperFavorite.displayedChild = when (favoriteUiState) {
+                DetailViewModel.FavoriteUiState.Loading -> FLIPPER_FAVORITE_CHILD_POSITION_LOADING
+                is DetailViewModel.FavoriteUiState.FavoriteIcon -> {
+                    binding.imageFavoriteIcon.setImageResource(favoriteUiState.icon)
+                    FLIPPER_FAVORITE_CHILD_POSITION_SUCCESS
+                }
+            }
+        }
     }
 
     // Define a animação da transição como "move"
@@ -91,5 +111,8 @@ class DetailFragment : Fragment() {
         private const val FLIPPER_CHILD_POSITION_DETAIL = 1
         private const val FLIPPER_CHILD_POSITION_ERROR = 2
         private const val FLIPPER_CHILD_POSITION_EMPTY = 3
+
+        private const val FLIPPER_FAVORITE_CHILD_POSITION_SUCCESS = 0
+        private const val FLIPPER_FAVORITE_CHILD_POSITION_LOADING = 1
     }
 }
