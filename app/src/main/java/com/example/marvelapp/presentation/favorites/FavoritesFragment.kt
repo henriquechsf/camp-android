a5052f9b27c22cb80e23950c4f68aeced5bf0518
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-12 00:31:43 -0300

    layout xml tela favoritos

--- app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentFavoritesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding: FragmentFavoritesBinding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        savedInstanceState: Bundle?,
+    ) = FragmentFavoritesBinding.inflate(inflater, container, false)
+        .apply { _binding = this }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
