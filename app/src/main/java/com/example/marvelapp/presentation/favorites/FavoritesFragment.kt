e5cc9aa76988f45d385ce377dfe0fefa404e4920
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-12 01:15:54 -0300

    generic adapter to list favorite characters

--- app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -6,13 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.marvelapp.databinding.FragmentFavoritesBinding
+import com.example.marvelapp.framework.imageloader.ImageLoader
+import com.example.marvelapp.presentation.common.getGenericAdapterOf
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding: FragmentFavoritesBinding get() = _binding!!
+
+    @Inject
+    private lateinit var imageLoader: ImageLoader
+
+    private val favoritesAdapter by lazy {
+        getGenericAdapterOf {
+            FavoritesViewHolder.create(it, imageLoader)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +34,15 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initFavoritesAdapter()
+    }
+
+    private fun initFavoritesAdapter() {
+        binding.recyclerFavorites.run {
+            setHasFixedSize(true)
+            adapter = favoritesAdapter
+        }
     }
 
     override fun onDestroy() {
