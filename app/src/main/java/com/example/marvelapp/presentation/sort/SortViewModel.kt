7316de4f8753daba2717d0d81bad462ec74d0e1b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-13 19:07:18 -0300

    cria feature ordenacao

---
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.marvelapp.presentation.sort
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SortViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
