60211e73c1c42051a591f9a7ef138454c1e10332
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-13 17:28:33 -0300

    refactor: GetCharactersUseCase

--- core/src/main/java/com/example/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/com/example/core/usecase/GetCharactersUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.core.usecase
 
-import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.core.data.repository.CharactersRepository
@@ -14,17 +13,16 @@ interface GetCharactersUseCase {
 
     data class Params(
         val query: String,
-        val pagingConfig: PagingConfig
+        val pagingConfig: PagingConfig,
     )
 }
 
 class GetCharactersUseCaseImpl @Inject constructor(
-    private val charactersRepository: CharactersRepository
+    private val charactersRepository: CharactersRepository,
 ) : PagingUseCase<GetCharactersUseCase.Params, Character>(),
     GetCharactersUseCase {
 
     override fun createFlowObservable(params: GetCharactersUseCase.Params): Flow<PagingData<Character>> {
-        val pagingSource = charactersRepository.getCharacters(params.query)
-        return Pager(config = params.pagingConfig) { pagingSource }.flow
+        return charactersRepository.getCachedCharacters(params.query, params.pagingConfig)
     }
 }
