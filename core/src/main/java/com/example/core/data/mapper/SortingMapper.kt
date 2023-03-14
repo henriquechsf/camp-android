b98e99fef9e8e298c8707adfc1643da9f559393b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-14 10:16:27 -0300

    armazena ordenacao no dataStore

--- core/src/main/java/com/example/core/data/mapper/SortingMapper.kt
+++ core/src/main/java/com/example/core/data/mapper/SortingMapper.kt
@@ -8,13 +8,13 @@ class SortingMapper @Inject constructor() {
 
     fun mapToPair(sorting: String): Pair<String, String> {
         val nameAscending =
-            SortingType.ORDER_BY_NAME.value to SortingType.ORDER_BY_ASCENDING.value
+            SortingType.ORDER_BY_NAME.value to SortingType.ORDER_ASCENDING.value
         val nameDescending =
-            SortingType.ORDER_BY_NAME.value to SortingType.ORDER_BY_DESCENDING.value
+            SortingType.ORDER_BY_NAME.value to SortingType.ORDER_DESCENDING.value
         val modifiedAscending =
-            SortingType.ORDER_BY_MODIFIED.value to SortingType.ORDER_BY_ASCENDING.value
+            SortingType.ORDER_BY_MODIFIED.value to SortingType.ORDER_ASCENDING.value
         val modifiedDescending =
-            SortingType.ORDER_BY_MODIFIED.value to SortingType.ORDER_BY_DESCENDING.value
+            SortingType.ORDER_BY_MODIFIED.value to SortingType.ORDER_DESCENDING.value
 
         return when (sorting) {
             StorageConstants.ORDER_BY_NAME_ASCENDING -> nameAscending
@@ -31,13 +31,13 @@ class SortingMapper @Inject constructor() {
 
         return when (orderBy) {
             SortingType.ORDER_BY_NAME.value -> when (order) {
-                SortingType.ORDER_BY_ASCENDING.value -> StorageConstants.ORDER_BY_NAME_ASCENDING
-                SortingType.ORDER_BY_DESCENDING.value -> StorageConstants.ORDER_BY_NAME_DESCENDING
+                SortingType.ORDER_ASCENDING.value -> StorageConstants.ORDER_BY_NAME_ASCENDING
+                SortingType.ORDER_DESCENDING.value -> StorageConstants.ORDER_BY_NAME_DESCENDING
                 else -> StorageConstants.ORDER_BY_NAME_ASCENDING
             }
             SortingType.ORDER_BY_MODIFIED.value -> when (order) {
-                SortingType.ORDER_BY_ASCENDING.value -> StorageConstants.ORDER_BY_MODIFIED_ASCENDING
-                SortingType.ORDER_BY_DESCENDING.value -> StorageConstants.ORDER_BY_MODIFIED_DESCENDING
+                SortingType.ORDER_ASCENDING.value -> StorageConstants.ORDER_BY_MODIFIED_ASCENDING
+                SortingType.ORDER_DESCENDING.value -> StorageConstants.ORDER_BY_MODIFIED_DESCENDING
                 else -> StorageConstants.ORDER_BY_MODIFIED_ASCENDING
             }
             else -> StorageConstants.ORDER_BY_NAME_ASCENDING
--- core/src/main/java/com/example/core/domain/model/SortingType.kt
+++ core/src/main/java/com/example/core/domain/model/SortingType.kt
@@ -3,6 +3,6 @@ package com.example.core.domain.model
 enum class SortingType(val value: String) {
     ORDER_BY_NAME("name"),
     ORDER_BY_MODIFIED("modified"),
-    ORDER_BY_ASCENDING("ascending"),
-    ORDER_BY_DESCENDING("descending"),
+    ORDER_ASCENDING("ascending"),
+    ORDER_DESCENDING("descending"),
 }
